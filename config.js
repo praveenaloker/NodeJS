187611c2b2e6205e07d5c8d198e68ee358fa0de7
Praveen Aloker <praveen.aloker@diaspark.com>
2020-08-12 12:57:50 +0530

    First comit

---
+++ config.js
@@ -0,0 +1,38 @@
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+var path= require('path');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var connection = new mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'password',
+    database:'user',
+});
+
+connection.connect((err)=>{
+    if(!err)
+    {
+        //console.log(err);
+        console.log("Connected !");
+    }
+    else
+    {
+        console.log(err);
+        console.log("Connection Failed !");
+    }
+});
+
+// app.use(express.static(__dirname + '/public'));
+// app.get("/", (req, res) => {
+//   res.sendFile('UserRegistration.html',{ root: __dirname })
+// });
+
+//app.use('/',require('./routes/pages'));
+
+//app.listen(3000);
